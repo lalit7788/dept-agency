65d9b49e8bec32821f325e68aa49052a34d99c1a
Lalit Mahapatra <lalit7788@gmail.com>
2023-01-23 19:46:49 +0100

    Filter on work categories and/or industries.

--- src/data/work.js
+++ src/data/work.js
@@ -2,6 +2,7 @@ const work = [
 	{
 		id: 1,
 		customer: "Bol.com",
+		categories: ["Shopping"],
 		industry: "E-commerce",
 		title: "A Summer island in the Netherlands",
 		imgName: "bol"
@@ -9,6 +10,7 @@ const work = [
 	{
 		id: 2,
 		customer: "Kempen",
+		categories: ["Finance"],
 		industry: "Banking",
 		title: "Not some average banking website",
 		imgName: "kempen"
@@ -16,6 +18,7 @@ const work = [
 	{
 		id: 3,
 		customer: "Philips",
+		categories: ["Lighting", "Smarthome"],
 		industry: "E-commerce",
 		title: "Beautiful design meets innovative technology",
 		imgName: "philips"
@@ -23,6 +26,7 @@ const work = [
 	{
 		id: 4,
 		customer: "Gemeentemuseum",
+		categories: ["History", "Culture"],
 		industry: "Government",
 		title: "100 years of Mondrian & De Stijl",
 		imgName: "gemeente-museum"
@@ -30,6 +34,7 @@ const work = [
 	{
 		id: 5,
 		customer: "Florensis",
+		categories: ["Shopping"],
 		industry: "E-commerce",
 		title: "Rethinking the entire online ecosystem",
 		imgName: "florensis"
@@ -37,18 +42,21 @@ const work = [
 	{
 		id: 6,
 		customer: "Microsoft",
+		categories: ["Gaming", "Lifestyle"],
 		industry: "Technology",
-		title: "Tapping into Ireland’s unique gaming culture and bringing a fresh flavour to their Xbox social media channels"
+		title: "Tapping into Ireland's unique gaming culture and bringing a fresh flavour to their Xbox social media channels"
 	},
 	{
 		id: 7,
 		customer: "O'neill",
+		categories: ["Shopping"],
 		industry: "E-commerce",
-		title: "Integrating existing content into O’Neills’s new e-commerce platform"
+		title: "Integrating existing content into O'Neills's new e-commerce platform"
 	},
 	{
 		id: 8,
 		customer: "Be Lightning",
+		categories: ["Business"],
 		industry: "Technology",
 		title: "Delivering clarity on a global scale",
 		imgName: "be-lightning"
@@ -56,6 +64,7 @@ const work = [
 	{
 		id: 9,
 		customer: "TUI",
+		categories: ["Holiday", "Airlines"],
 		industry: "Travel",
 		title: "Swipe to find your next holiday destination",
 		imgName: "tui"
@@ -63,12 +72,14 @@ const work = [
 	{
 		id: 10,
 		customer: "Amazon",
+		categories: ["Shopping"],
 		industry: "E-commerce",
 		title: "Starting with delivering through drones"
 	},
 	{
 		id: 11,
 		customer: "Florensis",
+		categories: ["Shopping"],
 		industry: "E-commerce",
 		title: "Rethinking the entire online ecosystem",
 		imgName: "florensis2"
@@ -76,6 +87,7 @@ const work = [
 	{
 		id: 12,
 		customer: "Chocomel",
+		categories: ["Eating"],
 		industry: "Food",
 		title: "A campaign for the limited edition letter packs",
 		imgName: "chocomel"
@@ -83,6 +95,7 @@ const work = [
 	{
 		id: 13,
 		customer: "JBL",
+		categories: ["Sound", "Lifestyle"],
 		industry: "Electronics",
 		title: "Live like a champion with Jerome Boateng",
 		imgName: "jbl"
@@ -90,6 +103,7 @@ const work = [
 	{
 		id: 14,
 		customer: "Zalando",
+		categories: ["Shopping", "Lifestyle"],
 		industry: "E-commerce",
 		title: "Innovative SEO and content strategy for Zalando",
 		imgName: "zalando"
@@ -97,13 +111,15 @@ const work = [
 	{
 		id: 15,
 		customer: "Koninklijke Bibliotheek",
-		industry: "Public",
+		categories: ["Books", "Culture"],
+		industry: "Government",
 		title: "The search of the most influential book ever",
 		imgName: "koninklijke-bibliotheek"
 	},
 	{
 		id: 16,
 		customer: "Transavia",
+		categories: ["Airlines"],
 		industry: "Travel",
 		quote: "Dept helped us tell our story through a bold new identity and a robust online experience. To the tone of 60% growth in online bookings in just one month",
 		signature: "Mattijs ten Drink - CEO"
@@ -111,15 +127,17 @@ const work = [
 	{
 		id: 17,
 		customer: "Liberty Global",
+		categories: ["Shopping", "Logistics"],
 		industry: "E-commerce",
 		title: "Delivering complex commerce solutions",
 		imgName: "liberty-global"
 	},
 	{
 		id: 18,
-		customer: "Bol.com",
+		customer: "Arla",
+		categories: ["Health", "Eating"],
 		industry: "Food",
-		title: "Swipe to find your next holiday destination",
+		title: "A healthier tomorrow starts today",
 		imgName: "arla"
 	}
 ];
--- src/views/WorkPortfolio.jsx
+++ src/views/WorkPortfolio.jsx
@@ -1,15 +1,177 @@
 import React from "react";
 import { Box } from "@mui/system";
-import { Grid } from "@mui/material";
+import { FormControl, Grid, MenuItem, Select, Typography } from "@mui/material";
 import WorkCard from "../components/WorkCard";
 import work from "../data/work";
+import styled from "styled-components";
+
+const FilterLabel = styled(Typography)(() => ({
+	color: "grey.600",
+	fontSize: 26
+}));
 
 const WorkPortfolio = function() {
+	const [selectedCategories, setSelectedCategories] = React.useState(["all-work"]);
+	const [selectedIndustries, setSelectedIndustries] = React.useState(["all-industries"]);
+
+	const workCategories = React.useMemo(() => {
+		return new Set(work
+			.filter((workItem) => {
+				if (selectedIndustries && selectedIndustries.indexOf("all-industries") === 0) {
+					return true;
+				}
+				return selectedIndustries.indexOf(workItem.industry) >= 0;
+			})
+			.flatMap((workItem) => workItem.categories)
+		);
+	}, [selectedIndustries]);
+
+	const workIndustries = React.useMemo(() => {
+		return new Set(work
+			.filter((workItem) => selectedCategories && selectedCategories.indexOf("all-work") === 0 ? true : workItem.categories.some((wCat) => selectedCategories.indexOf(wCat) >= 0))
+			.map((workItem) => workItem.industry)
+		);
+	}, [selectedCategories]);
+
+	const handleCategoriesSelect = (event) => {
+		const { value } = event.target;
+		if (value.length > 1) {
+			const allCategoriesIndex = value.indexOf("all-work");
+			if (allCategoriesIndex >= 0) {
+				if (allCategoriesIndex === 0) {
+					const newCategories = value.slice();
+					newCategories.splice(0, 1);
+					setSelectedCategories(newCategories);
+				} else {
+					setSelectedCategories(["all-work"]);
+				}
+			} else {
+				setSelectedCategories(value);
+			}
+		} else {
+			if (!value.length || value.indexOf("all-work") >= 0) {
+				setSelectedCategories(["all-work"]);
+			} else {
+				setSelectedCategories(value);
+			}
+		}
+	}
+
+	const handleIndustriesSelect = (event) => {
+		const { value } = event.target;
+		if (value.length > 1) {
+			const allCategoriesIndex = value.indexOf("all-industries");
+			if (allCategoriesIndex >= 0) {
+				if (allCategoriesIndex === 0) {
+					const newIndustries = value.slice();
+					newIndustries.splice(0, 1);
+					setSelectedIndustries(newIndustries);
+				} else {
+					setSelectedIndustries(["all-industries"]);
+				}
+			} else {
+				setSelectedIndustries(value);
+			}
+		} else {
+			if (!value.length || value.indexOf("all-industries") >= 0) {
+				setSelectedIndustries(["all-industries"]);
+			} else {
+				setSelectedIndustries(value);
+			}
+		}
+	}
+
 	return (<Box sx={{ overflowX: "hidden" }}>
-		<p>Filters here</p>
+		<Grid container alignItems="center" justifyContent="space-between" sx={{
+			px: 2,
+			py: 4
+		}}>
+			<Grid item sx={{ display: "inline-flex", alignItems: "center" }}>
+				<FilterLabel variant="h5">Show me</FilterLabel>
+				<FormControl
+					variant="standard"
+					sx={{
+						marginLeft: 1,
+						minWidth: 120
+					}}
+				>
+				<Select
+					multiple
+					value={selectedCategories}
+					onChange={handleCategoriesSelect}
+					sx={{
+						color: "grey.900",
+						backgroundColor: "common.white",
+						fontSize: 26,
+						"& .MuiSelect-icon": {
+							display: "none"
+						}
+					}}
+					MenuProps={{
+						sx: {
+							"& .MuiMenu-list": {
+								color: "grey.900",
+								backgroundColor: "common.white"
+							}
+						}
+					}}
+				>
+					<MenuItem value={"all-work"}>
+						all work
+					</MenuItem>
+					{Array.from(workCategories)
+						.map((category, index) => (<MenuItem key={index} value={category}>
+							{category}
+						</MenuItem>))}
+				</Select>
+				</FormControl>
+			</Grid>
+			<Grid item sx={{ display: "inline-flex", alignItems: "center" }}>
+				<FilterLabel variant="h5">in</FilterLabel>
+				<FormControl
+					variant="standard"
+					sx={{
+						marginLeft: 1,
+						minWidth: 150
+					}}
+				>
+				<Select
+					multiple
+					value={selectedIndustries}
+					onChange={handleIndustriesSelect}
+					sx={{
+						color: "grey.900",
+						backgroundColor: "common.white",
+						fontSize: 26,
+						"& .MuiSelect-icon": {
+							display: "none"
+						}
+					}}
+					MenuProps={{
+						sx: {
+							"& .MuiMenu-list": {
+								color: "grey.900",
+								backgroundColor: "common.white"
+							}
+						}
+					}}
+				>
+					<MenuItem value={"all-industries"}>
+						all industries
+					</MenuItem>
+					{Array.from(workIndustries)
+						.map((industry, index) => (<MenuItem key={index} value={industry}>
+							{industry}
+						</MenuItem>))}
+				</Select>
+				</FormControl>
+			</Grid>
+		</Grid>
 		<Grid container>
 			{work
 				.filter((workItem) => workItem.imgName)
+				.filter((workItem) => selectedCategories.indexOf("all-work") === 0 ? true : workItem.categories.some((wCat) => selectedCategories.indexOf(wCat) >= 0))
+				.filter((workItem) => selectedIndustries.indexOf("all-industries") === 0 ? true : selectedIndustries.indexOf(workItem.industry) >= 0)
 				.map(({ id, customer, title, imgName }) => (<Grid key={id} item xs={12} sm={12} md={6}>
 					<WorkCard
 						imgName={imgName}
